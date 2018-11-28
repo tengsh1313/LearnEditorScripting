6544b64e233e944c5467e38ca0ddd5cbd24916cc
tengsh1313 <tengsh1313@gmail.com>
2018-11-29 03:13:22 +0800

    CloneConsole, http://gram.gs/gramlog/creating-clone-unitys-console-window/

---
+++ CloneConsole/Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("1");
+        Debug.LogWarning("2");
+        Debug.LogError("3");
+        Debug.Log("4");
+        Debug.LogWarning("5");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
